7072df65eb0f8852d7e1a1ec2eeb8700df73c4c1
Li-Peiyan <87517767+Li-Peiyan@users.noreply.github.com>
2022-04-10 16:45:37 +0800

    Create sortedArrayToBST.java

---
+++ 0108. 将有序数组转换为二叉搜索树/sortedArrayToBST.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+
+    }
+    public TreeNode sortedArrayToBST(int[] nums, int left, int right) {
+        if(left > right ){
+            return null;
+        }
+        // 中间节点为根节点
+        int mid = (( right - left) >> 1) + left;
+
+        TreeNode root = new TreeNode( nums[mid] );
+
+        root.left = sortedArrayToBST(nums, left, mid-1);
+        root.right = sortedArrayToBST(nums, mid+1, right);
+
+        return root;
+    }
+}
